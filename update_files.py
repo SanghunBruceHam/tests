e57572aeab285ec39674c8bfe6440f6ed7341e1f
SanghunBruceHam <123874866+SanghunBruceHam@users.noreply.github.com>
2025-06-08 12:45:54 +0900

    Update update_files.py

--- update_files.py
+++ update_files.py
@@ -12,7 +12,6 @@ with open("robots.txt", "w", encoding="utf-8") as f:
 
 # 2. ads.txt
 ads_txt = """google.com, pub-5508768187151867, DIRECT, f08c47fec0942fa0
-google.com, pub-1111111111111111, DIRECT, f08c47fec0942fa0
 # Last updated: {}
 """.format(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC'))
 
